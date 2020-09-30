b16d6c934f860d6e62d3cb52373c857963ea12bb
Sylvain Ageneau <ageneau@gmail.com>
2020-09-30 08:28:42 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
             :year 2020
             :key "bsd-3-clause"}
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
                  [aysylu/loom "1.0.2"]
-                 [camel-snake-kebab "0.4.0"]]
+                 [camel-snake-kebab "0.4.1"]]
 
   :repositories [["github" "https://maven2.github.com"]
                  ["releases" {:url "https://repo.clojars.org"
@@ -17,11 +17,11 @@
 
   :profiles {:dev
              {:dependencies [[lein-doo "0.1.11"]
-                             [com.bhauman/figwheel-main "0.2.0"]
+                             [com.bhauman/figwheel-main "0.2.11"]
                              [com.bhauman/rebel-readline-cljs "0.1.4"]
-                             [com.taoensso/tufte "2.0.1"]
-                             [criterium "0.4.4"]
-                             [org.clojure/math.combinatorics "0.1.4"]]
+                             [com.taoensso/tufte "2.2.0"]
+                             [criterium "0.4.6"]
+                             [org.clojure/math.combinatorics "0.1.6"]]
               :plugins      [[lein-doo "0.1.11"]]
               :global-vars {;; *warn-on-reflection* true
                             ;; *unchecked-math* :warn-on-boxed
@@ -29,8 +29,8 @@
               :resource-paths ["test/resources"]}
              :bench
              {:main blossom.bench/-main
-              :dependencies [[com.taoensso/tufte "2.0.1"]
-                             [criterium "0.4.4"]]
+              :dependencies [[com.taoensso/tufte "2.2.0"]
+                             [criterium "0.4.6"]]
               :source-paths ["src" "test"]
               :resource-paths ["test/resources"]
               :global-vars {*warn-on-reflection* true
